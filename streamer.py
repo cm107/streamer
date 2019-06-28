4d0cf5550049967b091037f3bfce5be9d56217b3
Clayton Mork <mork.clayton3@gmail.com>
2019-06-28 11:39:34 +0900

    Added extra functionality and interface options to both Streamer and DualStreamer. I also simplified the code a little bit by introducing a streamer utilities.

--- streamer.py
+++ streamer.py
@@ -1,13 +1,18 @@
 import cv2
 import numpy as np
 from abc import ABCMeta, abstractmethod
+from .util.utils import scale_img
 
 class StreamerObject(metaclass=ABCMeta):
-    def __init__(self, src):
+    def __init__(self, src, scale_factor: float=1.0):
         self.worker = cv2.VideoCapture(src)
         self.full_width = None
+        self.full_height = None
         self.width = None
         self.height = None
+        self.scale_factor = scale_factor
+        self.downsized_width = None
+        self.downsized_height = None
     
     @abstractmethod
     def init_dims(self):
@@ -15,6 +20,10 @@ class StreamerObject(metaclass=ABCMeta):
 
     @abstractmethod
     def get_frames(self) -> np.ndarray:
+        pass
+
+    @abstractmethod
+    def _update_frame_buff(self, frame: np.ndarray) -> np.ndarray:
         pass
 
     def get_frame_count(self):
@@ -56,36 +65,71 @@ class StreamerObject(metaclass=ABCMeta):
         cv2.destroyAllWindows()
 
 class Streamer(StreamerObject):
-    def __init__(self, src):
-        super().__init__(src)
+    def __init__(self, src, scale_factor: float=1.0):
+        super().__init__(src, scale_factor)
         self.init_dims()
+        self.current_frame = None
 
     def init_dims(self):
         self.full_width = int(self.worker.get(3))
+        self.full_height = int(self.worker.get(4))
         self.width = self.full_width
-        self.height = int(self.worker.get(4))
+        self.height = self.full_height
+        self.downsized_width = int(self.width * self.scale_factor)
+        self.downsized_height = int(self.height * self.scale_factor)
 
     def get_frames(self) -> np.ndarray:
         rec, frame = self.worker.read()
         if rec:
-            return frame
+            self._update_frame_buff(frame)
+            return scale_img(frame, self.scale_factor)
         else:
             return None
 
+    def _update_frame_buff(self, frame: np.ndarray):
+        self.current_frame = frame if frame is not None else self.current_frame
+
 class DualStreamer(StreamerObject):
-    def __init__(self, src):
-        super().__init__(src)
+    def __init__(self, src, scale_factor: float=1.0, direction: int=0):
+        """
+        direction=0 : left-right direction
+        direction=1 : top-bottom direction
+        """
+
+        super().__init__(src, scale_factor)
+        self.direction = direction
+        self.init_dims()
+        self.current_left_frame = None
+        self.current_right_frame = None
 
     def init_dims(self):
-        self.full_width = self.worker.get(3)
-        self.width = int(self.full_width / 2)
-        self.height = self.worker.get(4)
+        self.full_width = int(self.worker.get(3))
+        self.full_height = int(self.worker.get(4))
+        if self.direction == 0: # left-right direction
+            self.width = int(self.full_width / 2)
+            self.height = self.full_height
+        elif self.direction == 1: # top-down direction
+            self.width = self.full_width
+            self.height = int(self.full_height / 2)
+        else:
+            raise Exception(f"Invalid direction: {self.direction}. Expecting direction in [0, 1]")
+        self.downsized_width = int(self.width * self.scale_factor)
+        self.downsized_height = int(self.height * self.scale_factor)
 
     def get_frames(self) -> np.ndarray:
         rec, frame = self.worker.read()
         if rec:
-            left_frame = frame[:,:self.width,:]
-            right_frame = frame[:,self.width:,:]
-            return left_frame, right_frame
+            if self.direction == 0:
+                left_frame = frame[:,:self.width,:]
+                right_frame = frame[:,self.width:,:]
+            elif self.direction == 1:
+                left_frame = frame[:self.height,:,:]
+                right_frame = frame[self.height:,:,:]
+            self._update_frame_buff(left_frame, right_frame)
+            return scale_img(left_frame, self.scale_factor), scale_img(right_frame, self.scale_factor)
         else:
             return None, None
+
+    def _update_frame_buff(self, left_frame: np.ndarray, right_frame: np.ndarray):
+        self.current_left_frame = left_frame if left_frame is not None else self.current_left_frame
+        self.current_right_frame = right_frame if right_frame is not None else self.current_right_frame
